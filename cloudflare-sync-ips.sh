320b45aa1f901689eeaaf21818bff4db79033603
Sávio Ribeiro <savio_rds@outlook.com>
2025-01-04 22:09:46 -0300

    Update cloudflare-sync-ips.sh
    Updated script to reload Nginx ONLY when Cloudflare IPs are successfully fetched and file is updated.
    
    * This prevents empty files that could lead to unintended firewall rules (e.g., with fail2ban) blocking Cloudflare IP ranges.

--- cloudflare-sync-ips.sh
+++ cloudflare-sync-ips.sh
@@ -2,22 +2,31 @@
 
 CLOUDFLARE_FILE_PATH=${1:-/etc/nginx/cloudflare}
 
-echo "#Cloudflare" > $CLOUDFLARE_FILE_PATH;
-echo "" >> $CLOUDFLARE_FILE_PATH;
+IPS_V4=$(curl -s -L https://www.cloudflare.com/ips-v4)
+IPS_V6=$(curl -s -L https://www.cloudflare.com/ips-v6)
 
-echo "# - IPv4" >> $CLOUDFLARE_FILE_PATH;
-for i in `curl -s -L https://www.cloudflare.com/ips-v4`; do
-        echo "set_real_ip_from $i;" >> $CLOUDFLARE_FILE_PATH;
-done
+if [ -n "$IPS_V4" ] && [ -n "$IPS_V6" ]; then
+    echo "#Cloudflare" > $CLOUDFLARE_FILE_PATH
+    echo "" >> $CLOUDFLARE_FILE_PATH
 
-echo "" >> $CLOUDFLARE_FILE_PATH;
-echo "# - IPv6" >> $CLOUDFLARE_FILE_PATH;
-for i in `curl -s -L https://www.cloudflare.com/ips-v6`; do
-        echo "set_real_ip_from $i;" >> $CLOUDFLARE_FILE_PATH;
-done
+    echo "# - IPv4" >> $CLOUDFLARE_FILE_PATH
+    for i in $IPS_V4; do
+        echo "set_real_ip_from $i;" >> $CLOUDFLARE_FILE_PATH
+    done
 
-echo "" >> $CLOUDFLARE_FILE_PATH;
-echo "real_ip_header CF-Connecting-IP;" >> $CLOUDFLARE_FILE_PATH;
+    echo "" >> $CLOUDFLARE_FILE_PATH
+    echo "# - IPv6" >> $CLOUDFLARE_FILE_PATH
+    for i in $IPS_V6; do
+        echo "set_real_ip_from $i;" >> $CLOUDFLARE_FILE_PATH
+    done
 
-#test configuration and reload nginx
-nginx -t && systemctl reload nginx
+    echo "" >> $CLOUDFLARE_FILE_PATH
+    echo "real_ip_header CF-Connecting-IP;" >> $CLOUDFLARE_FILE_PATH
+
+    # Test configuration and reload nginx
+    nginx -t && systemctl reload nginx
+    echo "File updated and Nginx reloaded successfully."
+else
+    echo "Error: Unable to get Cloudflare IPs (IPv4 or IPv6). The file was not updated." >&2
+    exit 1
+fi
